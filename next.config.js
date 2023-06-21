608ef585cdf0ff3f4cb25414916bca0a6b2103ed
josikie <kiejosi12@gmail.com>
2023-06-21 20:18:10 +0700

    Add authentications for user

---
+++ next.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+  trailingSlash: true,
+  reactStrictMode: false,
+  env: {
+    apiKey: "apikey",
+    authDomain: "authDomain",
+    projectId: "projectId",
+    storageBucket: "storageBucket",
+    messagingSenderId: "messagingSenderId",
+    appId: "appId"
+  },
+  experimental: {
+    esmExternals: false,
+    jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
+  },
+  webpack: config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
+    }
+
+    return config
+  }
+}
---
+++ src/auth/logout.js
@@ -0,0 +1,14 @@
+import { getAuth, signOut } from "firebase/auth";
+import firebase_app from "src/configs/firebase.config";
+
+const auth = getAuth(firebase_app);
+
+const logOut = () =>{
+    try{
+      signOut(auth)
+    }catch (e){
+      console.log(e)
+    }
+}
+
+export default logOut
